a5bd9161d852aa6477ff8c7a67b4249141817447
Sergey Rublev <sergey.rublev@dataspike.io>
2023-02-18 23:03:57 +0300

    Add document download api with tests
    Add filetype dependency for filetype upload

--- python/dataspike/documents/documents.py
+++ python/dataspike/documents/documents.py
@@ -1,10 +1,11 @@
 from typing import Optional
 from uuid import UUID
+import filetype
 
-from aiohttp import FormData
+from aiohttp import FormData, ClientResponse
 from pydantic import validate_arguments
 
-from .model import DocumentSide, DocumentType
+from .model import DocumentSide, DocumentType, Document
 from ..resource import Resource
 
 
@@ -18,7 +19,15 @@ class Documents(Resource):
         document_side: Optional[DocumentSide] = None,
     ) -> UUID:
         data = FormData()
-        data.add_field("file", file, content_type="image/*")
+        content_type = None
+        try:
+            content_type = filetype.guess(file)
+            if content_type:
+                content_type = content_type.mime
+        except TypeError:
+            pass
+
+        data.add_field("file", file, content_type=content_type)
         data.add_field("document_type", document_type)
         if document_side:
             data.add_field("side", document_side)
@@ -26,3 +35,25 @@ class Documents(Resource):
             await self._validate_resp(response, [200, 201], "upload document")
             data = await response.json()
             return UUID(data["document_id"])
+
+    @staticmethod
+    async def __get_document(response: ClientResponse) -> Document:
+        content_type = response.headers.get("Content-Type")
+        try:
+            document_type = DocumentType(response.headers.get("X-Document-Type"))
+        except ValueError:
+            document_type = None
+        content = await response.read()
+        return Document(content=content, content_type=content_type, document_type=document_type)
+
+    @validate_arguments
+    async def download(self, document_id: UUID) -> Document:
+        async with self._session.get(url=f"{self._api_endpoint}/api/v3/documents/{document_id}") as response:
+            await self._validate_resp(response, [200], "download document")
+            return await self.__get_document(response)
+
+    @validate_arguments
+    async def download_preview(self, document_id: UUID) -> Document:
+        async with self._session.get(url=f"{self._api_endpoint}/api/v3/documents/{document_id}/preview") as response:
+            await self._validate_resp(response, [200], "download document preview")
+            return await self.__get_document(response)
--- python/dataspike/documents/model.py
+++ python/dataspike/documents/model.py
@@ -1,8 +1,11 @@
+from typing import Optional
+
 from pydantic.dataclasses import dataclass
+from pydantic.fields import Field
 from enum import StrEnum
 from uuid import UUID
 
-__all__ = ["DocumentType", "DocumentSide", "DocumentRef"]
+__all__ = ["DocumentType", "DocumentSide", "DocumentRef", "Document"]
 
 
 class DocumentType(StrEnum):
@@ -42,3 +45,16 @@ class DocumentSide(StrEnum):
 class DocumentRef:
     document_id: UUID
     document_type: DocumentType
+
+
+@dataclass
+class Document:
+    content: bytes
+    document_type: Optional[DocumentType] = Field(default=None)
+    content_type: Optional[str] = Field(default=None)
+
+    def __repr__(self):
+        return (
+            f"DocumentContent(content_type={self.content_type}, "
+            f"document_type={self.document_type}, content=<{len(self.content)} bytes>"
+        )
