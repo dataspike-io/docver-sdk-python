1c4c09b8b099e12affbc405b3fd9a9cde76dace3
Sergey Rublev <sergey.rublev@dataspike.io>
2023-02-25 19:14:04 +0300

    Add AML api

--- python/dataspike/__init__.py
+++ python/dataspike/__init__.py
@@ -2,4 +2,5 @@ from .dataspike import Api
 from .applicants.model import *
 from .verifications.model import *
 from .documents.model import *
+from .aml.model import *
 from .common import *
--- python/dataspike/dataspike.py
+++ python/dataspike/dataspike.py
@@ -1,5 +1,6 @@
+import json
 from types import TracebackType
-from typing import Optional, Type
+from typing import Optional, Type, Any
 
 from aiohttp import ClientSession
 
@@ -7,6 +8,8 @@ from .applicants.applicants import Applicants
 from .documents.documents import Documents
 from .sdk.sdk import Sdk
 from .verifications.verifications import Verifications
+from .aml.aml import AML
+from .utils import DataspikeJsonEncoder
 
 __all__ = ["Api"]
 
@@ -14,6 +17,10 @@ from .__version__ import __version__
 
 
 class Api:
+    @staticmethod
+    def _encode_json(obj: Any) -> str:
+        return json.dumps(obj, cls=DataspikeJsonEncoder)
+
     def __init__(self, api_token: str, api_endpoint: str = "https://api.dataspike.io", **kwargs):
         """
         :param api_token: Organization API token
@@ -26,10 +33,11 @@ class Api:
             "ds-api-token": api_token,
             "User-Agent": f"dataspike-python/{__version__}",
         }
-        self._session = ClientSession(headers=default_headers, **kwargs)
+        self._session = ClientSession(headers=default_headers, json_serialize=self._encode_json, **kwargs)
         self.applicant: Applicants = Applicants(self._session, api_endpoint)
         self.verification = Verifications(self._session, api_endpoint)
         self.document = Documents(self._session, api_endpoint)
+        self.aml = AML(self._session, api_endpoint)
         self.sdk = Sdk(self._session, api_endpoint)
 
     def __repr__(self) -> str:
